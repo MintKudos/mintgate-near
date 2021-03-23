81b1f511991038f38aa8ac80f6cc90915853fc57
Luis Mastrangelo <luismastrangelo@gmail.com>
2021-03-23 14:16:28 +0100

    Add draft for register collectible

--- mg-core/src/lib.rs
+++ mg-core/src/lib.rs
@@ -1,31 +1,87 @@
-use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::{LookupMap, UnorderedMap, UnorderedSet};
-use near_sdk::serde::{Deserialize, Serialize};
-use near_sdk::{near_bindgen, AccountId, PanicOnDefault, StorageUsage};
+use near_env::near_envlog;
+use near_sdk::{
+    borsh::{self, BorshDeserialize, BorshSerialize},
+    collections::{LookupMap, UnorderedMap, UnorderedSet},
+    env,
+    json_types::U64,
+    near_bindgen,
+    serde::{Deserialize, Serialize},
+    AccountId,
+};
 
 #[global_allocator]
 static ALLOC: near_sdk::wee_alloc::WeeAlloc<'_> = near_sdk::wee_alloc::WeeAlloc::INIT;
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct Contract {
+    collectibles: UnorderedMap<TokenId, Collectible>,
+    collectibles_by_creator: LookupMap<AccountId, UnorderedSet<TokenId>>,
+    tokens_by_owner: LookupMap<AccountId, UnorderedSet<u128>>,
+}
 
 pub type TokenId = String;
 
 #[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize)]
 #[serde(crate = "near_sdk::serde")]
-pub struct Token {
-    pub owner_id: AccountId,
-    pub metadata: String,
+pub struct Collectible {
+    owner_id: AccountId,
+    title: String,
+    description: String,
+    supply: u64,
+    gate_url: String,
 }
 
+impl Default for Contract {
+    fn default() -> Self {
+        Self {
+            collectibles: UnorderedMap::new(vec![b'0']),
+            collectibles_by_creator: LookupMap::new(vec![b'1']),
+            tokens_by_owner: LookupMap::new(vec![b'2']),
+        }
+    }
+}
+
+#[near_envlog(skip_args, only_pub)]
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
-pub struct Contract {
-    pub tokens_per_owner: LookupMap<AccountId, UnorderedSet<TokenId>>,
+impl Contract {
+    pub fn create_collectible(
+        &mut self,
+        gate_id: String,
+        title: String,
+        description: String,
+        supply: U64,
+        gate_url: String,
+    ) {
+        let owner_id = env::predecessor_account_id();
 
-    pub tokens_by_id: UnorderedMap<TokenId, Token>,
+        let collectible = Collectible {
+            owner_id,
+            title,
+            description,
+            supply: supply.0,
+            gate_url,
+        };
+        self.collectibles.insert(&gate_id, &collectible);
 
-    pub owner_id: AccountId,
+        let mut gids = self
+            .collectibles_by_creator
+            .get(&collectible.owner_id)
+            .unwrap_or_else(|| UnorderedSet::new(get_key_prefix(b'c', &collectible.owner_id)));
+        gids.insert(&gate_id);
 
-    pub total_supply: u64,
+        self.collectibles_by_creator
+            .insert(&collectible.owner_id, &gids);
+    }
 
-    /// The storage size in bytes for one account.
-    pub extra_storage_in_bytes_per_token: StorageUsage,
+    pub fn claim_token(&mut self, gate_id: String) -> i32 {
+        0
+    }
+}
+
+fn get_key_prefix(prefix: u8, key: &String) -> Vec<u8> {
+    let mut key_prefix = Vec::with_capacity(33);
+    key_prefix.push(prefix);
+    key_prefix.extend(env::sha256(key.as_bytes()));
+    key_prefix
 }
