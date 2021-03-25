e6106a6ac6709f9870890eb9bdf3246e9fe5d65f
Luis Mastrangelo <luismastrangelo@gmail.com>
2021-03-25 16:49:17 +0100

    Add basic struct to support creation&claiming

--- mg-core/src/lib.rs
+++ mg-core/src/lib.rs
@@ -1,3 +1,8 @@
+#![deny(warnings)]
+
+pub mod fraction;
+
+use fraction::Fraction;
 use near_env::near_envlog;
 use near_sdk::{
     borsh::{self, BorshDeserialize, BorshSerialize},
@@ -5,46 +10,65 @@ use near_sdk::{
     env,
     json_types::U64,
     near_bindgen,
-    serde::{Deserialize, Serialize},
-    AccountId,
+    serde::Serialize,
+    AccountId, Balance, PanicOnDefault,
 };
 
 #[global_allocator]
 static ALLOC: near_sdk::wee_alloc::WeeAlloc<'_> = near_sdk::wee_alloc::WeeAlloc::INIT;
 
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
-    collectibles: UnorderedMap<TokenId, Collectible>,
-    collectibles_by_creator: LookupMap<AccountId, UnorderedSet<TokenId>>,
-    tokens_by_owner: LookupMap<AccountId, UnorderedSet<u128>>,
+    collectibles: UnorderedMap<GateId, Collectible>,
+    collectibles_by_creator: LookupMap<AccountId, UnorderedSet<GateId>>,
+    tokens: UnorderedMap<TokenId, Token>,
+    tokens_by_owner: LookupMap<AccountId, UnorderedSet<TokenId>>,
+    /// Percentage fee to pay back to Mintgate when a `Token` is being sold.
+    /// This field can be set up when the contract is deployed.
+    mintgate_fee: Fraction,
 }
 
-pub type TokenId = String;
+/// The `GateId` type represents the identifier of each `Collectible`.
+type GateId = String;
 
-#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize)]
+/// The `TokenId` type represents the identifier of each `Token`.
+type TokenId = u64;
+
+#[derive(BorshDeserialize, BorshSerialize, Serialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct Collectible {
-    owner_id: AccountId,
+    gate_id: GateId,
+    creator_id: AccountId,
     title: String,
     description: String,
-    supply: u64,
+    current_supply: u64,
     gate_url: String,
+    minted_tokens: Vec<TokenId>,
+    royalty: Fraction,
 }
 
-impl Default for Contract {
-    fn default() -> Self {
-        Self {
-            collectibles: UnorderedMap::new(vec![b'0']),
-            collectibles_by_creator: LookupMap::new(vec![b'1']),
-            tokens_by_owner: LookupMap::new(vec![b'2']),
-        }
-    }
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct Token {
+    token_id: TokenId,
+    gate_id: GateId,
+    owner_id: AccountId,
 }
 
 #[near_envlog(skip_args, only_pub)]
 #[near_bindgen]
 impl Contract {
+    #[init]
+    pub fn init(mintgate_fee: Fraction) -> Self {
+        Self {
+            collectibles: UnorderedMap::new(vec![b'0']),
+            collectibles_by_creator: LookupMap::new(vec![b'1']),
+            tokens: UnorderedMap::new(vec![b'2']),
+            tokens_by_owner: LookupMap::new(vec![b'3']),
+            mintgate_fee,
+        }
+    }
+
     pub fn create_collectible(
         &mut self,
         gate_id: String,
@@ -52,30 +76,57 @@ impl Contract {
         description: String,
         supply: U64,
         gate_url: String,
+        royalty: Fraction,
     ) {
-        let owner_id = env::predecessor_account_id();
+        let creator_id = env::predecessor_account_id();
 
         let collectible = Collectible {
-            owner_id,
+            gate_id,
+            creator_id,
             title,
             description,
-            supply: supply.0,
+            current_supply: supply.0,
             gate_url,
+            minted_tokens: Vec::new(),
+            royalty,
         };
-        self.collectibles.insert(&gate_id, &collectible);
+        self.collectibles.insert(&collectible.gate_id, &collectible);
 
         let mut gids = self
             .collectibles_by_creator
-            .get(&collectible.owner_id)
-            .unwrap_or_else(|| UnorderedSet::new(get_key_prefix(b'c', &collectible.owner_id)));
-        gids.insert(&gate_id);
+            .get(&collectible.creator_id)
+            .unwrap_or_else(|| UnorderedSet::new(get_key_prefix(b'c', &collectible.creator_id)));
+        gids.insert(&collectible.gate_id);
 
         self.collectibles_by_creator
-            .insert(&collectible.owner_id, &gids);
+            .insert(&collectible.creator_id, &gids);
     }
 
-    pub fn claim_token(&mut self, gate_id: String) -> i32 {
-        0
+    pub fn claim_token(&mut self, gate_id: String) -> U64 {
+        let owner_id = env::predecessor_account_id();
+
+        let token_id = self.tokens.len();
+        let token = Token {
+            token_id,
+            gate_id,
+            owner_id,
+        };
+        self.tokens.insert(&token.token_id, &token);
+
+        let mut tids = self
+            .tokens_by_owner
+            .get(&token.owner_id)
+            .unwrap_or_else(|| UnorderedSet::new(get_key_prefix(b't', &token.owner_id)));
+        tids.insert(&token.token_id);
+
+        self.tokens_by_owner.insert(&token.owner_id, &tids);
+
+        U64::from(token_id)
+    }
+
+    pub fn buy() -> u128 {
+        let a: Balance = 0;
+        a
     }
 }
 
