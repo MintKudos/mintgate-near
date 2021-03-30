7966a5b49c7b7ca68522418965ab8928d31673df
Aliaksandr Padhainy <sxurik@gmail.com>
2021-03-30 11:38:07 +0300

    Add declaration of core contract's methods

---
+++ lib/CoreMethods.d.ts
@@ -0,0 +1,4 @@
+export declare const CoreMethods: {
+    viewMethods: string[];
+    changeMethods: string[];
+};
---
+++ src/CoreMethods.ts
@@ -0,0 +1,4 @@
+export const CoreMethods = {
+  viewMethods: ['get_collectible_by_gate_id', 'get_collectibles_by_creator', 'get_tokens_by_owner'],
+  changeMethods: ['create_collectible', 'claim_token', 'buy', 'init'],
+};
