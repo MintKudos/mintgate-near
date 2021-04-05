3734d56511735f8d55bc69b3e2955350c7419cdf
Aliaksandr Padhainy <sxurik@gmail.com>
2021-04-05 16:14:57 +0300

    Add nft-contract's methods

--- src/methods.ts
+++ src/methods.ts
@@ -1,5 +1,12 @@
 export const NftMethods = {
-  viewMethods: ['get_collectible_by_gate_id', 'get_collectibles_by_creator', 'get_tokens_by_owner'],
+  viewMethods: [
+    'get_collectible_by_gate_id',
+    'get_collectibles_by_creator',
+    'nft_metadata',
+    'nft_total_supply',
+    'nft_token',
+    'get_tokens_by_owner',
+  ],
   changeMethods: [
     'init',
     'create_collectible',
